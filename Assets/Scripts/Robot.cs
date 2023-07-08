63e984aa58fc4ba3e28e68e58eba290a7c61815b
LAPTOP-7H4OOR82\Danielle <sisse008@umn.edu>
2023-07-08 19:35:19 +0300

    minor fixes and cleanups

--- Assets/Scripts/Robot.cs
+++ Assets/Scripts/Robot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,7 +7,7 @@ using UnityEngine.Events;
 public class Robot : MonoBehaviour
 {
 
-    public UnityAction<Robot> OnRobotSelected;
+    public Action<Robot> OnRobotSelected;
     private void OnMouseOver()
     {
         //highlight       
